45660d05c8d774ddbffbcda30e238d6eee80c659
Milton Figueredo Miles <gratno@gmail.com>
2024-03-10 11:02:13 -0500

    solution errors

--- src/main/java/com/websecurity/demo/jwt/Jwt/JwtService.java
+++ src/main/java/com/websecurity/demo/jwt/Jwt/JwtService.java
@@ -1,17 +1,36 @@
 package com.websecurity.demo.jwt.Jwt;
 
 import com.websecurity.demo.jwt.User.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET KEY="56856";
     public String getToken(UserDetails user) {
         return getToken(new HashMap<>(), user);
     }
 
-    private <V, K> String getToken(HashMap<K,V> kvHashMap, UserDetails user) {
+    private String getToken(Map<String,Object> extraClaims, UserDetails user) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey(), SignatureAlgorithm.ES256)
+                .compact();
+
+    }
+
+    private Key getKey() {
+        return null;
     }
 }
